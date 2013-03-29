8d5d727f4ff2a85797155c363b1fbdcdac16da13
Fedor Gogolev <knsd@knsd.net>
2013-03-30 03:35:53 +0400

    Git experiments

--- travis.sh
+++ travis.sh
@@ -10,10 +10,10 @@ cabal bench --benchmark-option="-onew-index.html"
 git config --global user.name "Travis CI"
 git config --global user.email "ci-bresson@knsd.net"
 
-git remote set-url origin https://${GH_TOKEN}@github.com/knsd/bresson.git
+rm -rf .git
 
-git remote update
-git fetch
+git clone https://${GH_TOKEN}@github.com/knsd/bresson.git
+
 git checkout -b gh-pages origin/gh-pages
 
 mv new-index.html index.html
