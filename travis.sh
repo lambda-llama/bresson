c13e206cffd8b35d679c4aeff16ce50e4ea9ec33
Fedor Gogolev <knsd@knsd.net>
2013-03-30 03:05:18 +0400

    Travis with gh-pages fixes

--- travis.sh
+++ travis.sh
@@ -3,14 +3,14 @@
 cabal configure --enable-tests --enable-benchmarks -fdevtools
 cabal build
 cabal test
-cabal bench --benchmark-option="-o new-index.html"
+cabal bench --benchmark-option="-onew-index.html"
 
 git config --global user.name "Travis CI"
-git config --global user.email "knsd@knsd.net"
+git config --global user.email "ci-bresson@knsd.net"
 
 git remote set-url origin https://${GH_TOKEN}@github.com/knsd/bresson.git
 
-git checkout -B gh-pages
+git checkout -b gh-pages origin/gh-pages
 
 mv new-index.html index.html
 
