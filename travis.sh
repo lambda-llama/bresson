180e9ee899bd5568d16f76f834714e3bc759d4b7
Fedor Gogolev <knsd@knsd.net>
2013-03-30 12:08:55 +0400

    Updated for storing all benchmarks in gh-pages

--- travis.sh
+++ travis.sh
@@ -5,21 +5,18 @@ set -e
 cabal configure --enable-tests --enable-benchmarks -fdevtools
 cabal build
 cabal test
-cabal bench --benchmark-option="-onew-index.html"
+
+cabal bench --benchmark-option='--output=$benchmark.html'
 
 git config --global user.name "Travis CI"
-git config --global user.email "ci-bresson@knsd.net"
-
-rm -rf .git
+git config --global user.email "ci+bresson@knsd.net"
 
 git clone https://${GH_TOKEN}@github.com/knsd/bresson.git
 
 cd bresson
-
 git checkout -b gh-pages origin/gh-pages
 
-mv ../new-index.html index.html
-
-git add index.html
+mv ../*.html .
+git add *.html
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
 git push
