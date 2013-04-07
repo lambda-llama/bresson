439efebd7f62d0533a2af8f0b3781e78e2552ddf
Sergei Lebedev <superbobry@gmail.com>
2013-04-08 01:28:20 +0400

    'AgainstBson' now uses real data, shamelessly stolen from 'aeson'

--- benchmarks/AgainstBson.hs
+++ benchmarks/AgainstBson.hs
@@ -1,26 +1,20 @@
 {-# LANGUAGE PackageImports #-}
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE CPP #-}
 
 module Main where
 
 import Control.DeepSeq (NFData(..), force)
-import Control.Monad (replicateM)
 import Data.ByteString.Lazy (ByteString)
 import Data.Binary (decode, encode)
 import Data.Binary.Get (runGet)
 import Data.Binary.Put (runPut)
-import System.Random (StdGen, newStdGen)
-#if !MIN_VERSION_bytestring(0,10,0)
 import qualified Data.ByteString.Lazy as L
-#endif
 
-import Criterion.Main (defaultMain, bench, nf)
+import Criterion.Main (defaultMain, bgroup, bench, nf)
 import Control.DeepSeq.TH (deriveNFDatas)
-import Test.QuickCheck (arbitrary, resize)
-import Test.QuickCheck.Gen (unGen)
 import "bresson" Data.Bson.Binary ()
 import "bresson" Data.Bson.Tests.Instances ()
 import qualified "bresson" Data.Bson as Bresson
@@ -43,32 +37,37 @@ deriving instance NFData Bson.MongoStamp
 $(deriveNFDatas [''Bson.Value, ''Bson.MinMaxKey, ''Bson.Regex,
                  ''Bson.Javascript, ''Bson.ObjectId, ''Bson.Field])
 
-encodeBresson :: [Bresson.BsonDocument] -> [ByteString]
-encodeBresson = map encode
+encodeBresson :: Bresson.BsonDocument -> ByteString
+encodeBresson = encode
+{-# INLINE encodeBresson #-}
 
-decodeBresson :: [ByteString] -> [Bresson.BsonDocument]
-decodeBresson = map decode
+decodeBresson :: ByteString -> Bresson.BsonDocument
+decodeBresson = decode
+{-# INLINE decodeBresson #-}
 
-encodeBson :: [Bson.Document] -> [ByteString]
-encodeBson = map (runPut . Bson.putDocument)
+encodeBson :: Bson.Document -> ByteString
+encodeBson = runPut . Bson.putDocument
+{-# INLINE encodeBson #-}
 
-decodeBson :: [ByteString] -> [Bson.Document]
-decodeBson = map (runGet Bson.getDocument)
-
-generate :: Int -> Int -> IO ByteString
-generate size seed = newStdGen >>= \gen ->
-    return $ encode $ mkDoc gen
-  where
-    mkDoc :: StdGen -> Bresson.BsonDocument
-    mkDoc gen = unGen (resize size arbitrary) gen seed
+decodeBson :: ByteString -> Bson.Document
+decodeBson = runGet Bson.getDocument
+{-# INLINE decodeBson #-}
 
 main :: IO ()
 main = do
-    testData <- replicateM 100 $ generate 100 42
+    testData <- L.readFile "benchmarks/bson-data/twitter100.bson"
     let bson = force $ decodeBson testData
     let bresson = force $ decodeBresson testData
-    defaultMain [ bench "decode bson" $ nf decodeBson testData
-                , bench "decode bresson" $ nf decodeBresson testData
-                , bench "encode bson" $ nf encodeBson bson
-                , bench "encode bresson" $ nf encodeBresson bresson
+    defaultMain [ bgroup "encode"
+                  [ bgroup "twitter-100"
+                    [ bench "bson" $ nf encodeBson bson
+                    , bench "bresson" $ nf encodeBresson bresson
+                    ]
+                  ]
+                , bgroup "decode"
+                  [ bgroup "twitter-100"
+                    [ bench "bson" $ nf decodeBson testData
+                    , bench "bresson" $ nf decodeBresson testData
+                    ]
+                  ]
                 ]
