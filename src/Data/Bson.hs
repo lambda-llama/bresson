6c8275123fadfdf1a3412e1eee9af4b21064e135
Fedor Gogolev <knsd@knsd.net>
2014-03-12 09:28:10 +0400

    Move parser stuff to a separate file

--- src/Data/Bson.hs
+++ src/Data/Bson.hs
@@ -1,5 +1,6 @@
 module Data.Bson
     ( module Data.Bson.Class
+    , module Data.Bson.Parser
     , module Data.Bson.Types
     , module Data.Bson.Utils
     ) where
@@ -8,6 +9,7 @@ module Data.Bson
 import Data.Bson.Binary ()
 
 import Data.Bson.Class (ToBson(..), FromBson(..))
+import Data.Bson.Parser (Parser, parse, parseMaybe, parseEither)
 import Data.Bson.Types (Document, Label, Value(..), Binary(..), ObjectId(..),
 						Array, RegexOption(..), RegexOptions)
 import Data.Bson.Utils (document, (!?), (=:))
